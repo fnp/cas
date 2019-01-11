ba4a79354412a4679b7a24358c9a09fc60ea78c2
Radek Czajka <rczajka@rczajka.pl>
2019-01-11 21:33:46 +0100

    Changes from server.

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -16,6 +16,7 @@ def user_attributes(sender, user, **kwargs):
     return {
         'firstname': user.first_name,
         'lastname': user.last_name,
+        'cn': ' '.join((user.first_name, user.last_name)),
         'email': user.email,
     }
 cas_collect_custom_attributes.connect(user_attributes)
