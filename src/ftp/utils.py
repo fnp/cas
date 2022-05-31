47c02a4c678e46e0bebb354a6c16ab1d4022efe5
Radek Czajka <rczajka@rczajka.pl>
2022-05-31 15:55:07 +0200

    change password generator

--- src/ftp/utils.py
+++ src/ftp/utils.py
@@ -1,6 +1,5 @@
 import secrets
-import string
+
 
 def generate_password(length=10):
-    chars = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(secrets.choice(chars) for i in range(length))
+    return secrets.token_urlsafe(length)
